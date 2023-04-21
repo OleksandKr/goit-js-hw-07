c3a9e1fab5a1ecebda2dfa02475278654bb45a7c
OleksandKr <cfif357@ukr.net>
2023-04-21 13:07:54 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+import * as basicLightbox from "basiclightbox";
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`);
+
+instance.show();
+
+const galleryEl = document.querySelector(".gallery");
+console.log(galleryEl);
+
+const makeGaleryEl = ({ preview, original, description }) => {
+  return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       data-source="${original}"
+       alt="${description}"
+     />
+   </a>
+ </li>`;
+};
+
+const addingTagsGalery = galleryItems.map(makeGaleryEl).join("");
+galleryEl.insertAdjacentHTML("afterbegin", addingTagsGalery);
+
+galleryEl.addEventListener("click", onClickGallery);
+
+const onClickGallery = (event) => {
+
+};
+
+
+
+
